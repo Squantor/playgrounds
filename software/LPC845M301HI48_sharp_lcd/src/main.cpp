700a666164c244dc3c1918f9b5b326b8035a7b16
Squantor <squantmuts@gmail.com>
2021-12-10 17:39:34 +0100

    updated submodules, added LCD demo code

--- software/LPC845M301HI48_sharp_lcd/src/main.cpp
+++ software/LPC845M301HI48_sharp_lcd/src/main.cpp
@@ -10,8 +10,9 @@
  * its effect, you'll need to use debugger.
  */
 #include <nuclone_LPC845M301BD48_sharp_lcd.hpp>
+#include <sharp_memlcd.hpp>
 
-volatile int var;
+volatile unsigned int systicks = 0;
 volatile bool pinmode;
 
 extern "C" 
@@ -19,15 +20,91 @@ extern "C"
     void SysTick_Handler(void)
     {
         gpioPortToggle(GPIO, PORT_LED, 1 << PIN_LED);
+        systicks = systicks + 1;
     }
 }
+
+void waitSpiTxComplete(void)
+{
+    while(!(spiSetGetStatus(SPI0, 0x0) & SPI_STAT_TXRDY))
+        ;
+}
+
+void lcdTransfer(uint16_t *begin, uint16_t *end)
+{
+    // transfer of 1
+    if(begin+1 == end)
+    {
+        waitSpiTxComplete();
+        spiSetTxCtrlData(SPI0,  SPI_TXDATCTL_TXDAT(*begin) | 
+        SPI_TXDATCTL_TXSSEL0 | 
+        SPI_TXDATCTL_EOF |
+        SPI_TXDATCTL_EOT |
+        SPI_TXDATCTL_RXIGNORE | 
+        SPI_TXDATCTL_LEN(16) );
+        return ;
+    }
+    // transfer of N
+    else
+    {
+        uint16_t *p = begin;
+        while (p < end)
+        {
+            waitSpiTxComplete();
+            spiSetTxCtrlData(SPI0,  SPI_TXDATCTL_TXDAT(*p) | 
+            SPI_TXDATCTL_TXSSEL0 | 
+            SPI_TXDATCTL_EOF |
+            SPI_TXDATCTL_RXIGNORE | 
+            SPI_TXDATCTL_LEN(16) );
+            p++;
+        }
+        // line or multiline transfers need to be terminated
+        waitSpiTxComplete();
+        spiSetTxCtrlData(SPI0,  SPI_TXDATCTL_TXDAT(0x0000) | 
+            SPI_TXDATCTL_TXSSEL0 | 
+            SPI_TXDATCTL_EOF |
+            SPI_TXDATCTL_EOT |
+            SPI_TXDATCTL_RXIGNORE | 
+            SPI_TXDATCTL_LEN(16) );    
+    }
+}
+
+util::sharpMemLcd<util::LS013B4DN04> boardLcd;
 
 int main()
 {
     pinmode = false;
+    unsigned int currticks = systicks;
+    boardInit();
+    spiSetTxCtrlData(SPI0,  SPI_TXDATCTL_TXDAT(0x4) | 
+        SPI_TXDATCTL_TXSSEL0 | 
+        SPI_TXDATCTL_EOF |
+        SPI_TXDATCTL_RXIGNORE | 
+        SPI_TXDATCTL_LEN(3) );
+    waitSpiTxComplete();
+    spiSetTxCtrlData(SPI0,  SPI_TXDATCTL_TXDAT(0x0000) | 
+        SPI_TXDATCTL_TXSSEL0 | 
+        SPI_TXDATCTL_EOF |
+        SPI_TXDATCTL_EOT |
+        SPI_TXDATCTL_RXIGNORE | 
+        SPI_TXDATCTL_LEN(16) );
     boardInit();
     while (1) {
-        var = var ^ 0x55;
-        __NOP();
+        if(currticks < systicks)
+        {
+            currticks = systicks;
+            for(unsigned int i = 0 ; i < boardLcd.maxY; i++)
+            {
+                for(unsigned int j = 0; j < boardLcd.maxX; j++)
+                {
+                    boardLcd.putPixel(j, i, ((j ^ i) + currticks) & 0x20);
+                    //boardLcd.putPixel(i, j, ((i*i + j*j) + currticks) & 0x20);
+                    //if(i == j) boardLcd.putPixel(i, j, 1);
+                }
+            }
+            //transferFramebuf();
+            boardLcd.lcdUpdate(lcdTransfer);
+            boardLcd.flipVcom(lcdTransfer);
+        }
     }
 }
--- software/LPC845M301HI48_sharp_lcd/src/nuclone_LPC845M301BD48.cpp
+++ software/LPC845M301HI48_sharp_lcd/src/nuclone_LPC845M301BD48.cpp
@@ -4,7 +4,7 @@
  * Copyright (c) 2021 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
  */
-#include <nuclone_LPC845M301BD48_sharp_lcd.hpp>>
+#include <nuclone_LPC845M301BD48_sharp_lcd.hpp>
 
 void crudeDelay(uint32_t iterations)
 {
@@ -24,11 +24,14 @@ void crudeDelay(uint32_t iterations)
 void boardInit(void)
 {
     sysconEnableClocks(SYSCON, CLKCTRL0_IOCON | CLKCTRL0_GPIO0 | CLKCTRL0_SWM, CLKCTRL1_NONE);
+    ioconSetupPin(IOCON, IOCON_DISP_CE, IOCON_MODE_INACTIVE);
+    ioconSetupPin(IOCON, IOCON_DISP_MOSI, IOCON_MODE_INACTIVE);
+    ioconSetupPin(IOCON, IOCON_DISP_SCK, IOCON_MODE_INACTIVE);
     ioconSetupPin(IOCON, IOCON_LED, IOCON_MODE_INACTIVE);
     // setup crystal oscillator and PLL to run core at 30MHz
     ioconSetupPin(IOCON, IOCON_XTAL_IN, IOCON_MODE_INACTIVE);
     ioconSetupPin(IOCON, IOCON_XTAL_OUT, IOCON_MODE_INACTIVE);
-    swmEnableFixedPin(SWM0, SWM_EN0_XTALIN | SWM_EN0_XTALOUT, SWM_EN1_NONE);
+    swmEnableFixedPin(SWM, SWM_EN0_XTALIN | SWM_EN0_XTALOUT, SWM_EN1_NONE);
     sysconSysOscControl(SYSCON, SYSOSCCTRL_FREQ_1_20MHZ);
     sysconPowerEnable(SYSCON, PDRUNCFG_SYSOSC);
     crudeDelay(6000);
@@ -50,4 +53,15 @@ void boardInit(void)
     sysconDisableClocks(SYSCON, CLKCTRL0_SWM, CLKCTRL1_NONE);
     // setup systick
     SysTick_Config(CLOCK_AHB / TICKS_PER_S);
+
+    // setup SPI
+    SwmMovablePinAssign(SWM, SWM_SPI0_SSEL0, SWM_DISP_CE);
+    SwmMovablePinAssign(SWM, SWM_SPI0_MOSI, SWM_DISP_MOSI);
+    SwmMovablePinAssign(SWM, SWM_SPI0_SCK, SWM_DISP_SCK);
+    sysconPeripheralClockSelect(SYSCON, SPI0CLKSEL, CLKSRC_MAIN);
+    spiSetDivider(SPI0, 60);
+    spiSetConfig(SPI0, SPI_CFG_ENABLE | SPI_CFG_MASTER | SPI_CFG_LSBF | SPI_CFG_SPOL0);
+
+    // disable all unneeded clocks
+    sysconDisableClocks(SYSCON, CLKCTRL0_SWM, CLKCTRL1_NONE);
 }
