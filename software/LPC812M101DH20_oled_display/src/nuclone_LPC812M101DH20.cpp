46ff6ff452df94b25c28ba2a0b796c6ff6ff0549
Squantor <squantmuts@gmail.com>
2022-01-12 13:30:36 +0100

    added beginnings of I2C handling

--- software/LPC812M101DH20_oled_display/src/nuclone_LPC812M101DH20.cpp
+++ software/LPC812M101DH20_oled_display/src/nuclone_LPC812M101DH20.cpp
@@ -37,6 +37,7 @@ void crudeDelay(uint32_t iterations) {
 }
 
 void boardInit(void) {
+  // setup io pins
   sysconEnableClocks(SYSCON, CLKCTRL_SWM | CLKCTRL_IOCON | CLKCTRL_GPIO | CLKCTRL_SCT | CLKCTRL_I2C0);
   ioconSetupPin(IOCON, IOCON_XTAL_IN, IOCON_MODE(PIN_INACTIVE));
   ioconSetupPin(IOCON, IOCON_XTAL_OUT, IOCON_MODE(PIN_INACTIVE));
@@ -46,8 +47,8 @@ void boardInit(void) {
   swmEnableFixedPin(SWM, SWM_EN0_XTALIN | SWM_EN0_XTALOUT);
   sysconDisableClocks(SYSCON, CLKCTRL_SWM | CLKCTRL_IOCON);
   SwmMovablePinAssign(SWM, SWM_SCT_OUT_0_O, SWM_LED);
+
   // setup system clocks
-  // ClockSetPLLBypass(false, false);
   sysconSysOscControl(SYSCON, SYSOSCCTRL_BYPASS(0) | SYSOSCCTRL_FREQ_1_20MHZ);
   sysconPowerEnable(SYSCON, PDRUNCFG_SYSOSC);
   crudeDelay(6000);
@@ -55,16 +56,19 @@ void boardInit(void) {
   FmcSetFlashAccess(FLASHTIM_30MHZ_CPU);
   sysconPowerDisable(SYSCON, PDRUNCFG_SYSPLL);
   sysconPllControl(SYSCON, 4, SYSPLLCTRL_POSTDIV_4);
-  //  SysctlPowerUp(SYSCTL_SLPWAKE_SYSPLL_PD);
   sysconPowerEnable(SYSCON, PDRUNCFG_SYSPLL);
   while (!sysconPllStatus(SYSCON))
     ;
   sysconMainClockDivider(SYSCON, 2);
-  // ClockSetMainClockSource(SYSCTL_MAINCLKSRC_PLLOUT);
   sysconMainClockSelect(SYSCON, MAINCLKSEL_PLL_OUT);
+
+  // setup led pin
   gpioSetPinDIROutput(GPIO, PORT_LED, PIN_LED);
   gpioPinWrite(GPIO, PORT_LED, PIN_LED, 0);
   SysTick_Config(CLOCK_AHB / TICKS_PER_S);
 
   // setup i2c
+  sysconAssertResets(SYSCON, RESETCTRL_I2C);
+  sysconClearResets(SYSCON, RESETCTRL_I2C);
+  i2cSetClockDivider(I2C0, 5);
 }
