ccbf2bdc455128f019b4ecb94694042456a2157a
Squantor <squantmuts@gmail.com>
2022-12-23 19:55:56 +0100

    WIP PLL setup

--- software/projects/RP2040_demo/src/generic_board.cpp
+++ software/projects/RP2040_demo/src/generic_board.cpp
@@ -7,17 +7,67 @@
 #include <generic_board.hpp>
 
 void boardInit(void) {
-  // Enable XOSC
-  XOSC->CTRL = XOSC_CTRL_DIS | XOSC_CTRL_FREQ_RANGE_1_15MHZ;
-  XOSC->STARTUP = 47;  // ~1 ms @ 12 MHz
-  XOSC->CTRL = XOSC_CTRL_EN | XOSC_CTRL_FREQ_RANGE_1_15MHZ;
-  while (0 == (XOSC->STATUS & XOSC_STATUS_ENABLED_MASK))
+  volatile uint32_t timeout = 0;
+  (void)timeout;
+  // ~1 ms @ 12 MHz
+  timeout = xoscStart(47, 0x1000000);
+
+  // Setup SYS PLL for 12 MHz * 40 / 4 / 1 = 120 MHz
+  timeout = resetsReset(RESETS_PLL_SYS_MASK, 0x1000000);
+  timeout = pllSysStart(1, 40, 4, 1, 0x1000000);
+  /*
+
+  PLL_SYS->CS = (1 << PLL_SYS_CS_REFDIV_Pos);
+  PLL_SYS->FBDIV_INT = 40;
+  PLL_SYS->PRIM = (4 << PLL_SYS_PRIM_POSTDIV1_Pos) | (1 << PLL_SYS_PRIM_POSTDIV2_Pos);
+
+  PLL_SYS_CLR->PWR = PLL_SYS_PWR_VCOPD_Msk | PLL_SYS_PWR_PD_Msk;
+  while (0 == PLL_SYS->CS_b.LOCK)
     ;
 
-  resetsReset(RESETS_CLR, RESETS_IO_BANK0_MASK | RESETS_PADS_BANK0_MASK);
-  while (resetsGetResetDone(RESETS, RESETS_IO_BANK0_MASK | RESETS_PADS_BANK0_MASK) ==
-         (RESETS_IO_BANK0_MASK | RESETS_PADS_BANK0_MASK))
+  PLL_SYS_CLR->PWR = PLL_SYS_PWR_POSTDIVPD_Msk;
+
+  // Setup USB PLL for 12 MHz * 36 / 3 / 3 = 48 MHz
+  RESETS_CLR->RESET = RESETS_RESET_pll_usb_Msk;
+  while (0 == RESETS->RESET_DONE_b.pll_usb)
     ;
+
+  PLL_USB->CS = (1 << PLL_SYS_CS_REFDIV_Pos);
+  PLL_USB->FBDIV_INT = 36;
+  PLL_USB->PRIM = (3 << PLL_SYS_PRIM_POSTDIV1_Pos) | (3 << PLL_SYS_PRIM_POSTDIV2_Pos);
+
+  PLL_USB_CLR->PWR = PLL_SYS_PWR_VCOPD_Msk | PLL_SYS_PWR_PD_Msk;
+  while (0 == PLL_USB->CS_b.LOCK)
+    ;
+
+  PLL_USB_CLR->PWR = PLL_SYS_PWR_POSTDIVPD_Msk;
+
+  // Switch clocks to their final socurces
+  CLOCKS->CLK_REF_CTRL = (CLOCKS_CLK_REF_CTRL_SRC_xosc_clksrc << CLOCKS_CLK_REF_CTRL_SRC_Pos);
+
+  CLOCKS->CLK_SYS_CTRL = (CLOCKS_CLK_SYS_CTRL_AUXSRC_clksrc_pll_sys << CLOCKS_CLK_SYS_CTRL_AUXSRC_Pos);
+  CLOCKS_SET->CLK_SYS_CTRL = (CLOCKS_CLK_SYS_CTRL_SRC_clksrc_clk_sys_aux << CLOCKS_CLK_SYS_CTRL_SRC_Pos);
+
+  CLOCKS->CLK_PERI_CTRL =
+    CLOCKS_CLK_PERI_CTRL_ENABLE_Msk | (CLOCKS_CLK_PERI_CTRL_AUXSRC_clk_sys << CLOCKS_CLK_PERI_CTRL_AUXSRC_Pos);
+
+  CLOCKS->CLK_USB_CTRL =
+    CLOCKS_CLK_USB_CTRL_ENABLE_Msk | (CLOCKS_CLK_USB_CTRL_AUXSRC_clksrc_pll_usb << CLOCKS_CLK_USB_CTRL_AUXSRC_Pos);
+
+  CLOCKS->CLK_ADC_CTRL =
+    CLOCKS_CLK_ADC_CTRL_ENABLE_Msk | (CLOCKS_CLK_ADC_CTRL_AUXSRC_clksrc_pll_usb << CLOCKS_CLK_ADC_CTRL_AUXSRC_Pos);
+
+  CLOCKS->CLK_RTC_DIV = (256 << CLOCKS_CLK_RTC_DIV_INT_Pos);  // 12MHz / 256 = 46875 Hz
+  CLOCKS->CLK_RTC_CTRL =
+    CLOCKS_CLK_RTC_CTRL_ENABLE_Msk | (CLOCKS_CLK_RTC_CTRL_AUXSRC_xosc_clksrc << CLOCKS_CLK_ADC_CTRL_AUXSRC_Pos);
+
+  // Configure 1 us tick for watchdog and timer
+  WATCHDOG->TICK = ((F_REF / F_TICK) << WATCHDOG_TICK_CYCLES_Pos) | WATCHDOG_TICK_ENABLE_Msk;
+
+  */
+
+  timeout = resetsReset(RESETS_IO_BANK0_MASK | RESETS_PADS_BANK0_MASK, 0x1000000);
+
   // setup LED pin
   sioGpioOeSet(SIO, LED_MASK);
   iobank0GpioCtrl(IO_BANK0, LED_PIN, BANK0_GPIO25_FUNC_SIO, 0);
