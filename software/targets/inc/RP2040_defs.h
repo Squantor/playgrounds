ee406c5401f61def015cf250675080ecb93073c9
Squantor <squantmuts@gmail.com>
2022-12-31 13:59:26 +0100

    quadspi read mode works

--- software/targets/inc/RP2040_defs.h
+++ software/targets/inc/RP2040_defs.h
@@ -25,6 +25,10 @@ For conditions of distribution and use, see LICENSE file
 #define SSI_CTRLR1_OFFSET (0x04)     /**< SSI control register 1 offset */
 #define SSI_SSIENR_OFFSET (0x08)     /**< SSI enable  register offset */
 #define SSI_BAUDR_OFFSET (0x14)      /**< SSI baudrate register offset */
+#define SSI_SR_OFFSET (0x28)         /**< SSI status register offset */
+#define SSI_SR_TFE_BITS (1 << 2)     /**< SSI status TX Fifo empty bit */
+#define SSI_SR_BUSY_BITS (1 << 0)    /**< SSI status busy bit */
+#define SSI_DR0_OFFSET (0x60)        /**< SSI Data register 0 */
 #define SSI_SPI_CTRLR0_OFFSET (0xf4) /**< SSI SPI control register 0 offset */
 #define M0PLUS_VTOR_OFFSET (0xed08)  /**< Cortex M0 VTOR offset register */
 
