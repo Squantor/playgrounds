2a71283a2a7b82513bee6a735c0c6be6f4305d0c
Bart Bilos <squantmuts+git@gmail.com>
2023-01-27 12:11:56 +0100

    initial simple single project PC template

---
+++ software/singleproject_c_cpp_PC/inc/test.h
@@ -0,0 +1,20 @@
+/*
+SPDX-License-Identifier: MIT
+
+Copyright (c) 2021 Bart Bilos
+For conditions of distribution and use, see LICENSE file
+*/
+#ifndef TEST_H
+#define TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int testfunc(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
