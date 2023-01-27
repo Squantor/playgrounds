865656140cae1e2d5d6f55ee9fdab3856f39c694
Bart Bilos <squantmuts+git@gmail.com>
2023-01-27 11:15:54 +0100

    UART RX+TX work

--- software/projects/RP2040_demo/src/main.cpp
+++ software/projects/RP2040_demo/src/main.cpp
@@ -11,6 +11,7 @@
  */
 #include <nuclone_RP2040.hpp>
 
+uint8_t helloString[] = "Hello World\n";
 volatile uint32_t systicks = 0;
 
 extern "C" {
@@ -32,16 +33,16 @@ __attribute__((noinline, section(".ramfunc"))) void delay_cycles(uint32_t cycles
 int main() {
   static uint32_t currTicks = 0;
   boardInit();
+  uartWriteBlocking(UART0, helloString, sizeof(helloString));
   while (1) {
     delay_cycles(10);
     __NOP();
     if (currTicks != systicks) {
-      uint8_t dataWrite = (uint8_t)(currTicks & 0xFF);
-      uartWriteBlocking(UART0, &dataWrite, sizeof(dataWrite));
-      uint8_t dataRead = 0;
-      uartReadBlocking(UART0, &dataRead, sizeof(dataRead));
-      if (dataRead != dataWrite)
-        __NOP();
+      if (0 == uartIsRxAvailable(UART0)) {
+        uint8_t dataRead = 0;
+        uartReadBlocking(UART0, &dataRead, sizeof(dataRead));
+        uartWriteBlocking(UART0, &dataRead, sizeof(dataRead));
+      }
       sioGpioOutXor(SIO, LED_MASK);
       currTicks = systicks;
     }
