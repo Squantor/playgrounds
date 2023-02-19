7c24e4fbb45c9334f7d3fff43fd1df04a4173620
Bart Bilos <squantmuts+git@gmail.com>
2023-02-19 10:45:34 +0100

    moving oled stuff from lpc812 demo to RP2040

--- software/multiproject_c_cpp/projects/RP2040_oled_display/src/main.cpp
+++ software/multiproject_c_cpp/projects/RP2040_oled_display/src/main.cpp
@@ -10,6 +10,10 @@
  * its effect, you'll need to use debugger.
  */
 #include <nuclone_RP2040.hpp>
+#include <SSD1306_direct.hpp>
+#include <graphicalconsole.hpp>
+#include <font_8x8.h>
+#include <print.h>
 
 volatile uint32_t systicks = 0;
 const uint8_t helloString[] = "Hello World\n";
@@ -30,24 +34,43 @@ __attribute__((noinline, section(".ramfunc"))) void delay_cycles(uint32_t cycles
     : [cycles] "+l"(cycles));
 }
 
+SSD1306::display<0x78, SSD1306::standard128x64> currentDisplay;
+graphicalConsole<SSD1306::standard128x64::maxX, SSD1306::standard128x64::maxY> currentConsole(mono8x8RowFlip);
+
+void currentDisplayWriteWindow(uint8_t xBegin, uint8_t xEnd, uint8_t yBegin, uint8_t yEnd, const uint8_t *data, uint16_t length) {
+  currentDisplay.writeWindow(xBegin, xEnd, yBegin, yEnd, data, length);
+}
+
+result currentDisplayWriteChar(const char *c) {
+  currentConsole.writeBigChar(currentDisplayWriteWindow, *c);
+  return noError;
+}
+
+const datastreamChar_t displayStream{
+  .write = currentDisplayWriteChar,
+  .read = NULL,
+  .name = NULL,
+};
+
 int main() {
-  static uint32_t currTicks = 0;
+  uint32_t currentTicks = 0;
   boardInit();
-  uartWriteBlocking(UART0, helloString, sizeof(helloString));
+  currentDisplay.init();
+  dsPuts(&displayStream, "Hello World!");
   while (1) {
     delay_cycles(10);
     __NOP();
     if (currTicks != systicks) {
-      if (0 == uartIsRxAvailable(UART0)) {
-        uint8_t dataRead = 0;
-        uint16_t spiDataOut = 0, spiDataIn;
-        uartReadBlocking(UART0, &dataRead, sizeof(dataRead));
-        dataRead++;
-        uartWriteBlocking(UART0, &dataRead, sizeof(dataRead));
-        spiDataIn = dataRead + currTicks;
-        spiTranceiveBits(SPI0, &spiDataIn, &spiDataOut, 1, 16);
-        __NOP();
-      }
+      printDecNzU32(&displayStream, currentTicks);
+      dsWriteChar(&displayStream, currentTicks & 0x7F);
+      currentTicks = ticks;
+      uint8_t invertDisplay;
+      if (currentTicks & 16)
+        invertDisplay = SSD1306::displayInvert;
+      else
+        invertDisplay = SSD1306::displayNormal;
+      currentDisplay.sendCommands(&invertDisplay, sizeof(invertDisplay));
+      currentDisplay.update();
       sioGpioOutXor(SIO, LED_MASK);
       currTicks = systicks;
     }
