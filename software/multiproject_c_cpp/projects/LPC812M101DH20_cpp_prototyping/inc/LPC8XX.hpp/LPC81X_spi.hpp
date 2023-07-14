5caa10c83dd349338b38153c905e00431e4f0510
Bart Bilos <squantmuts+git@gmail.com>
2023-07-14 08:38:54 +0200

    ported over all LPC844 functions, added TODO functions

--- software/multiproject_c_cpp/projects/LPC812M101DH20_cpp_prototyping/inc/LPC8XX.hpp/LPC81X_spi.hpp
+++ software/multiproject_c_cpp/projects/LPC812M101DH20_cpp_prototyping/inc/LPC8XX.hpp/LPC81X_spi.hpp
@@ -10,9 +10,21 @@
 #ifndef LPC81X_SPI_HPP
 #define LPC81X_SPI_HPP
 
+#include <span>
+
 namespace instances {
 namespace spi {
+
 using namespace registers::spi;
+
+/**
+ * @brief SPI hardware chip enables
+ *
+ */
+enum chipEnables : uint32_t {
+  SSEL = (1 << 16), /**< Hardware chip enable */
+};
+
 template <uint32_t base>
 struct spi {
   /**
@@ -38,6 +50,72 @@ struct spi {
     return actualBitRate;
   }
 
+  // TODO: initMaster with alternative SSEL polarities
+  // TODO: initMaster with CPOL/CPHA and SSEL polarities
+  // TODO: initSlave();
+  // TODO: initSlave with alternative SSEL polarities
+  // TODO: initSlave with CPOL/CPHA and SSEL polarities
+
+  /**
+   * @brief Transmit data to SPI
+   *
+   * @param device SPI device to transmit to
+   * @param transmitBuffer data to transmit
+   * @param bitcount amount of bits to transmit
+   * @param lastAction is this the last action? This will disable the chip select
+   */
+  void transmit(chipEnables device, const std::span<uint16_t> transmitBuffer, uint32_t bitcount, bool lastAction) {
+    size_t index = 0;
+    uint32_t baseTransferCommand = TXDATCTL::TXSSEL(device) | TXDATCTL::RXIGNORE;  // base transfer command with presets
+    while (bitcount > 16) {
+      regs()->TXDATCTL = baseTransferCommand | TXDATCTL::TXDAT(transmitBuffer[index]) | TXDATCTL::LEN(16);
+      while ((regs()->STAT & STAT::TXRDY) == 0)
+        ;
+      bitcount -= 16;
+      index++;
+    }
+    // process remainder
+    if (lastAction)
+      baseTransferCommand |= TXDATCTL::EOT;
+    regs()->TXDATCTL = baseTransferCommand | TXDATCTL::TXDAT(transmitBuffer[index]) | TXDATCTL::LEN(bitcount);
+    while ((regs()->STAT & STAT::TXRDY) == 0)
+      ;
+  }
+
+  // TODO: transmit with gpio chip select
+  // TODO: transmit with chip select lambda
+
+  /**
+   * @brief Receive data from SPI, transmitter is active but transmits zeroes, disable MOSI if you want to float it
+   *
+   * @param device SPI device to receive data from
+   * @param receiveBuffer buffer to put data into
+   * @param bitcount amount of bits to receive
+   * @param lastAction is this the last action? This will disable the chip select
+   */
+  void receive(chipEnables device, std::span<uint16_t> receiveBuffer, uint32_t bitcount, bool lastAction) {
+    size_t index = 0;
+    uint32_t baseTransferCommand = TXDATCTL::TXSSEL(device);  // base transfer command with presets
+    while (bitcount > 16) {
+      regs()->TXDATCTL = baseTransferCommand | TXDATCTL::LEN(16);
+      while ((regs()->STAT & STAT::RXRDY) == 0)
+        ;
+      receiveBuffer[index] = RXDAT::RXDAT(regs()->RXDAT);
+      bitcount -= 16;
+      index++;
+    }
+    // process remainder
+    if (lastAction)
+      baseTransferCommand |= TXDATCTL::EOT;
+    regs()->TXDATCTL = baseTransferCommand | TXDATCTL::LEN(bitcount);
+    while ((regs()->STAT & STAT::RXRDY) == 0)
+      ;
+    receiveBuffer[index] = RXDAT::RXDAT(regs()->RXDAT);
+  }
+
+  // TODO: receive with gpio chip select
+  // TODO: receive with chip select lambda
+
   /**
    * @brief Set the SPI peripheral bit rate
    *
@@ -52,6 +130,40 @@ struct spi {
     regs()->DIV = DIV::DIVVAL(divider);
     return CLOCK_AHB / divider;
   }
+
+  /**
+   * @brief Transmit and recieve data via SPI
+   *
+   * @param device SPI device to use
+   * @param transmitBuffer data to transmit
+   * @param receiveBuffer buffer to put data into
+   * @param bitcount amount of bits
+   * @param lastAction is this the last action? This will disable the chip select
+   */
+  void transceive(chipEnables device, const std::span<uint16_t> transmitBuffer, std::span<uint16_t> receiveBuffer,
+                  uint32_t bitcount, bool lastAction) {
+    size_t index = 0;
+    uint32_t baseTransferCommand = TXDATCTL::TXSSEL(device);  // base transfer command with presets
+    while (bitcount > 16) {
+      regs()->TXDATCTL = baseTransferCommand | TXDATCTL::TXDAT(transmitBuffer[index]) | TXDATCTL::LEN(16);
+      while ((regs()->STAT & STAT::RXRDY) == 0)
+        ;
+      receiveBuffer[index] = RXDAT::RXDAT(regs()->RXDAT);
+      bitcount -= 16;
+      index++;
+    }
+    // process remainder
+    if (lastAction)
+      baseTransferCommand |= TXDATCTL::EOT;
+    regs()->TXDATCTL = baseTransferCommand | TXDATCTL::TXDAT(transmitBuffer[index]) | TXDATCTL::LEN(bitcount);
+    while ((regs()->STAT & STAT::RXRDY) == 0)
+      ;
+    receiveBuffer[index] = RXDAT::RXDAT(regs()->RXDAT);
+  }
+
+  // TODO: transceive with gpio chip select
+  // TODO: transceive with chip select lambda
+  // TODO: configure delay settings
 };
 }  // namespace spi
 }  // namespace instances
--- software/multiproject_c_cpp/projects/LPC812M101DH20_cpp_prototyping/inc/LPC8XX.hpp/LPC81X_spi_regs.hpp
+++ software/multiproject_c_cpp/projects/LPC812M101DH20_cpp_prototyping/inc/LPC8XX.hpp/LPC81X_spi_regs.hpp
@@ -30,6 +30,7 @@ struct registers {
   volatile uint32_t DIV;           /**< SPI clock Divider register */
   volatile const uint32_t INTSTAT; /**< SPI Interrupt Status register */
 };
+
 namespace CFG {
 constexpr inline uint32_t ENABLE = (1 << 0); /**< SPI enable */
 constexpr inline uint32_t MASTER = (1 << 2); /**< master mode */
@@ -40,9 +41,11 @@ constexpr inline uint32_t CPOL = (1 << 5);   /**< Clock polarity select */
 constexpr inline uint32_t LOOP = (1 << 7);   /**< Loopback mode enable */
 constexpr inline uint32_t SPOL = (1 << 8);   /**< SSEL0 Polarity select */
 }  // namespace CFG
+
 namespace DLY {
 // TODO register definitions
 }
+
 namespace STAT {
 constexpr inline uint32_t RXRDY = (1 << 0);       /**< Receiver Ready flag. When 1 data available  */
 constexpr inline uint32_t TXRDY = (1 << 1);       /**< Transmitter Ready flag. When 1 data can be written */
@@ -54,10 +57,13 @@ constexpr inline uint32_t STALLED = (1 << 6);     /**< Stalled status flag. */
 constexpr inline uint32_t ENDTRANSFER = (1 << 7); /**< End Transfer control bit. Force end of current transaction */
 constexpr inline uint32_t MSTIDLE = (1 << 8);     /**< Master idle status flag. Master is fully idle */
 }  // namespace STAT
+
 namespace INTENCLR {
 // TODO register definitions
 }
+
 namespace RXDAT {
+
 /**
  * @brief Format received data from RXDAT register field
  *
@@ -67,10 +73,13 @@ namespace RXDAT {
 constexpr inline uint16_t RXDAT(uint32_t registerData) {
   return static_cast<uint16_t>(registerData & 0xFFFF);
 }
+
 constexpr inline uint32_t RXSSEL_N = (1 << 16); /**< Slave 0 selected for receive transaction, zero is active */
 constexpr inline uint32_t SOT = (1 << 20);      /**< Start of transfer flag. 1 when SSEL is asserted the first time */
 }  // namespace RXDAT
+
 namespace TXDATCTL {
+
 /**
  * @brief Formats data to TXDATCTL register field
  *
@@ -80,10 +89,22 @@ namespace TXDATCTL {
 constexpr inline uint32_t TXDAT(uint16_t data) {
   return static_cast<uint32_t>(data);
 }
+
 constexpr inline uint32_t TXSSEL_N = (1 << 16); /**< This field asserts SSEL0 in master mode.*/
+/**
+ * @brief Formats chipselects to TXSSEL register field
+ *
+ * @param chipselects chipselects to use
+ * @return constexpr uint32_t properly formatted chipselects to be put in register
+ */
+constexpr inline uint32_t TXSSEL(uint32_t chipselects) {
+  return chipselects ^ 0x000F0000u;
+}
+
 constexpr inline uint32_t EOT = (1 << 20);      /**< End of Transfer. Deassert SSEL */
 constexpr inline uint32_t EOF = (1 << 21);      /**< End of Frame. Insert delay between frames */
 constexpr inline uint32_t RXIGNORE = (1 << 22); /**< Receive ignore */
+
 /**
  * @brief Format data transmit length to TXDATCTL register field
  *
@@ -93,21 +114,35 @@ constexpr inline uint32_t RXIGNORE = (1 << 22); /**< Receive ignore */
 constexpr inline uint32_t LEN(uint32_t length) {
   return ((length - 1) & 0x0F) << 24;
 }
+
 }  // namespace TXDATCTL
+
 namespace TXDAT {
 // TODO register definitions
 }
+
 namespace TXCTL {
 // TODO register definitions
 }
+
 namespace DIV {
+
+/**
+ * @brief Formats divider value to DIVVAL register field
+ *
+ * @param divider clock divider value
+ * @return constexpr uint32_t properly formatted divider to be put in register
+ */
 constexpr inline uint32_t DIVVAL(uint16_t divider) {
   return divider - 1;  // subtract one according to datasheet
 }
+
 }  // namespace DIV
+
 namespace INTSTAT {
 // TODO register definitions
 }
+
 }  // namespace spi
 }  // namespace registers
 
