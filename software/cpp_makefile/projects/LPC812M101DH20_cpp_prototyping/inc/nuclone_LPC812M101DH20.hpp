36ba9a06d8084dc75bfc8c50b42fdb3cdcb3202b
Bart Bilos <squantmuts+git@gmail.com>
2023-08-18 12:22:50 +0200

    moving C++ frames to libmcull

--- software/cpp_makefile/projects/LPC812M101DH20_cpp_prototyping/inc/nuclone_LPC812M101DH20.hpp
+++ software/cpp_makefile/projects/LPC812M101DH20_cpp_prototyping/inc/nuclone_LPC812M101DH20.hpp
@@ -17,32 +17,6 @@ For conditions of distribution and use, see LICENSE file
 
 #include <mcu_ll.hpp>
 
-#include <LPC8XX.hpp/LPC81X_acmp_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_crc_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_fmc_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_i2c_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_mrt_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_pin_int_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_pmu_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_sct_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_syscon_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_usart_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_wkt_regs.hpp>
-#include <LPC8XX.hpp/LPC81X_wwdt_regs.hpp>
-
-#include <LPC8XX.hpp/LPC81X_acmp.hpp>
-#include <LPC8XX.hpp/LPC81X_crc.hpp>
-#include <LPC8XX.hpp/LPC81X_fmc.hpp>
-#include <LPC8XX.hpp/LPC81X_i2c.hpp>
-#include <LPC8XX.hpp/LPC81X_mrt.hpp>
-#include <LPC8XX.hpp/LPC81X_pin_int.hpp>
-#include <LPC8XX.hpp/LPC81X_pmu.hpp>
-#include <LPC8XX.hpp/LPC81X_sct.hpp>
-#include <LPC8XX.hpp/LPC81X_syscon.hpp>
-#include <LPC8XX.hpp/LPC81X_usart.hpp>
-#include <LPC8XX.hpp/LPC81X_wkt.hpp>
-#include <LPC8XX.hpp/LPC81X_wwdt.hpp>
-
 #define TICKS_PER_S 10
 
 // main gpio port
