f999073c06abeec88f11cab95d48d04321a669e3
Bart Bilos <squantmuts+git@gmail.com>
2024-03-15 11:09:36 +0100

    WIP testing out things

--- software/projects/include_test/src/main.cpp
+++ software/projects/include_test/src/main.cpp
@@ -7,10 +7,20 @@ For conditions of distribution and use, see LICENSE file
 #include <cstdint>
 #include <cstdio>
 
-#define xstr(s) str(s)
-#define str(s) #s
+// Macro for adding quotes
+#define STRINGIFY(X) STRINGIFY2(X)
+#define STRINGIFY2(X) #X
+
+// Macros for concatenating tokens
+#define CAT(X, Y) CAT2(X, Y)
+#define CAT2(X, Y) X##Y
+#define CAT_2 CAT
+#define CAT_3(X, Y, Z) CAT(X, CAT(Y, Z))
+#define CAT_4(A, X, Y, Z) CAT(A, CAT_3(X, Y, Z))
+
+#define INCLUDE_FILE(ARGUMENT) STRINGIFY(CAT_3(ARGUMENT, ., hpp))
 
 int main() {
-  printf("test string %s\n", xstr(TEST));
+  printf("test string %s\n", STRINGIFY(INCLUDE_FILE(broccoli)));
   return 0;
 }
