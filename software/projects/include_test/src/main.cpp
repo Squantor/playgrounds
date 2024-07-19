496e06eac079e81811452c7e3a63326c88266381
Bart Bilos <squantmuts+git@gmail.com>
2024-07-19 09:48:16 +0200

    updates

--- software/projects/include_test/src/main.cpp
+++ software/projects/include_test/src/main.cpp
@@ -15,14 +15,21 @@ For conditions of distribution and use, see LICENSE file
 #define CAT(X, Y) CAT2(X, Y)
 #define CAT2(X, Y) X##Y
 #define CAT_2 CAT
-#define CAT_3(X, Y, Z) CAT(X, CAT(Y, Z))
-#define CAT_4(A, X, Y, Z) CAT(A, CAT_3(X, Y, Z))
+#define CAT_3(X, Y, Z) CAT_2(X, CAT_2(Y, Z))
+#define CAT_4(A, X, Y, Z) CAT_2(A, CAT_3(X, Y, Z))
 
-#define INCLUDE_FILE(ARGUMENT) STRINGIFY(CAT2(ARGUMENT, _m.hpp))
+#define APP_STRING()
 
+#define INCLUDE_FILE(ARGUMENT) STRINGIFY(CAT_2(ARGUMENT, _m.hpp))
+
+// #if __has_include("sub/broccoli_m.hpp")
 #include INCLUDE_FILE(DEVICE)
+/*#else
+#error "unknown include"
+#endif*/
 
 int main() {
-  printf("test string %s\n", DEVICE_TYPE);
+  // printf("test string %s\n", DEVICE_TYPE);
+  printf("include file string %s\n", INCLUDE_FILE(DEVICE));
   return 0;
 }
