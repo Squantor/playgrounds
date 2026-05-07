6e609efa898b67103c54de3b275e5532de4478d6
Bart Bilos <squantmuts@gmail.com>
2026-05-07 18:22:42 +0200

    added blit functions operating on bitmap and associated tests

--- standalone_software/blits.+23/blit_bitmap.cpp
+++ standalone_software/blits.+23/blit_bitmap.cpp
@@ -19,11 +19,40 @@
 #include "bitmap.hpp"
 
 void blit_bitmap(Bitmap dst, Const_bitmap src, Bitmap_coord dst_x, Bitmap_coord dst_y, Blit_ops op) {
-  (void)dst;
-  (void)src;
-  (void)dst_x;
-  (void)dst_y;
-  (void)op;
+  if (dst_y > dst.get_height() || dst_x > dst.get_width()) {
+    return;
+  }
+  // clamp bit count depending on destination bitmap size
+  std::size_t src_width_bit_count{0};
+  if (dst_x + src.get_width() > dst.get_width()) {
+    src_width_bit_count = static_cast<std::size_t>(dst.get_width() - dst_x) * static_cast<std::size_t>(src.get_bits_per_pixel());
+  } else {
+    src_width_bit_count = static_cast<std::size_t>(src.get_width()) * static_cast<std::size_t>(src.get_bits_per_pixel());
+  }
+
+  std::size_t src_span_size{(static_cast<std::size_t>(src.get_width()) * static_cast<std::size_t>(src.get_height()) *
+                             static_cast<std::size_t>(src.get_bits_per_pixel())) /
+                            32};
+  std::size_t dst_span_size{(static_cast<std::size_t>(dst.get_width()) * static_cast<std::size_t>(dst.get_height()) *
+                             static_cast<std::size_t>(dst.get_bits_per_pixel())) /
+                            32};
+  std::span src_span(src.data(), src_span_size);
+  std::span dst_span(dst.data(), dst_span_size);
+  std::size_t src_bit_stride{static_cast<std::size_t>(src.get_width()) * static_cast<std::size_t>(src.get_bits_per_pixel())};
+  std::size_t dst_bit_stride{static_cast<std::size_t>(dst.get_width()) * static_cast<std::size_t>(dst.get_bits_per_pixel())};
+  std::size_t src_bit_size{src_span_size * 32};
+  std::size_t src_bit_index{0};
+  std::size_t dst_bit_index{static_cast<std::size_t>(dst_y * dst.get_width() + dst_x) *
+                            static_cast<std::size_t>(dst.get_bits_per_pixel())};
+  while (src_bit_index < src_bit_size) {
+    blit_1d_bits(dst_span, src_span, dst_bit_index, src_bit_index, src_width_bit_count, op);
+    src_bit_index += src_bit_stride;
+    dst_bit_index += dst_bit_stride;
+    //! @todo change source bit size to match destination to get this check out of the loop
+    if (dst_bit_index > dst_span_size * 32) {
+      break;
+    }
+  }
 }
 
 void blit_bitmap(Bitmap dst, Const_bitmap src, Bitmap_coord dst_x, Bitmap_coord dst_y, Bitmap_coord src_x, Bitmap_coord src_y,
--- standalone_software/blits.+23/tests/bitmap_blit_tests.cpp
+++ standalone_software/blits.+23/tests/bitmap_blit_tests.cpp
@@ -11,14 +11,49 @@
 #include <array>
 #include <bitmap.hpp>
 #include <minunit.h>
+#include <blits.hpp>
 
 std::array<const std::uint32_t, 8> dut_bitmap_src_data{
   {0x33221100, 0x77665544, 0xBBAA9988, 0xFFEEDDCC, 0x1F2E3D4C, 0x5F6E7D8C, 0x9FAEBDBC, 0xFFEEDDCC}};
-std::array<std::uint32_t, 8> dut_bitmap_dst_data;
+std::array<std::uint32_t, 12> dut_bitmap_dst_data;
 
 namespace {
-// Copy a 4 bit bitmap to a 4 bit bitmap on a specific location
+
+MINUNIT_ADD(test_bitmap_blit_inside, nullptr, nullptr) {
+  Const_bitmap dut_bitmap_src(dut_bitmap_src_data.data(), 4, 4, 4);
+  Bitmap dut_bitmap_dst(dut_bitmap_dst_data.data(), 8, 8, 4);
+  dut_bitmap_dst_data.fill(0xCCCC5555);
+  blit_bitmap(dut_bitmap_dst, dut_bitmap_src, Bitmap_coord{2}, Bitmap_coord{2});
+  MINUNIT_CHECK(dut_bitmap_dst_data[1] == 0xCCCC5555);
+  MINUNIT_CHECK(dut_bitmap_dst_data[2] == 0xCC110055);
+  MINUNIT_CHECK(dut_bitmap_dst_data[3] == 0xCC332255);
+  MINUNIT_CHECK(dut_bitmap_dst_data[4] == 0xCC554455);
+  MINUNIT_CHECK(dut_bitmap_dst_data[5] == 0xCC776655);
+  MINUNIT_CHECK(dut_bitmap_dst_data[6] == 0xCCCC5555);
+}
+
+MINUNIT_ADD(test_bitmap_blit_outside, nullptr, nullptr) {
+  Const_bitmap dut_bitmap_src(dut_bitmap_src_data.data(), 4, 4, 4);
+  Bitmap dut_bitmap_dst(dut_bitmap_dst_data.data(), 8, 8, 4);
+  dut_bitmap_dst_data.fill(0xCCCC5555);
+  blit_bitmap(dut_bitmap_dst, dut_bitmap_src, Bitmap_coord{8}, Bitmap_coord{8});
+  for (const auto &element : dut_bitmap_dst_data) {
+    MINUNIT_CHECK(element == 0xCCCC5555);
+  }
+}
+
+MINUNIT_ADD(test_bitmap_blit_outside_corner, nullptr, nullptr) {
+  Const_bitmap dut_bitmap_src(dut_bitmap_src_data.data(), 4, 4, 4);
+  Bitmap dut_bitmap_dst(dut_bitmap_dst_data.data(), 8, 8, 4);
+  dut_bitmap_dst_data.fill(0xCCCC5555);
+  blit_bitmap(dut_bitmap_dst, dut_bitmap_src, Bitmap_coord{6}, Bitmap_coord{6});
+  MINUNIT_CHECK(dut_bitmap_dst_data[5] == 0xCCCC5555);
+  MINUNIT_CHECK(dut_bitmap_dst_data[6] == 0x00CC5555);
+  MINUNIT_CHECK(dut_bitmap_dst_data[7] == 0x22CC5555);
+  MINUNIT_CHECK(dut_bitmap_dst_data[8] == 0xCCCC5555);
+}
+
 // Copy a bounded 4 bit bitmap to a 4 bit bitmap on a specific location
 // Copy a 1 bit bitmap to a 1 bit bitmap on a specific location
 // Copy a bounded 1 bit bitmap to a 1 bit bitmap on a specific location
-}
+}  // namespace
