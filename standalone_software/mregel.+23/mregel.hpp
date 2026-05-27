50b6b4c74c1a037ae25bebc022ea9a58e23d6f1c
Bart Bilos <squantmuts@gmail.com>
2026-05-27 21:53:28 +0200

    added variadic template for add function and its tests

--- standalone_software/mregel.+23/mregel.hpp
+++ standalone_software/mregel.+23/mregel.hpp
@@ -4,7 +4,7 @@
  * Copyright (c) 2025 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
  * @file mregel.hpp
- * @brief mregel class definitions
+ * @brief Mregel class definitions
  */
 #ifndef MREGEL_HPP
 #define MREGEL_HPP
@@ -12,25 +12,40 @@
 #include <span>
 
 /**
+ * @brief Defines a hex value, used to convert a hex value to a string
+ */
+struct Hex {
+  std::uint32_t v;
+};
+/**
+ * @brief Defines a decimal value, used to convert a decimal value to a string
+ */
+struct Dec {
+  std::int32_t v;
+};
+/**
  * @brief
  */
-class mregel {
+namespace detail {
+const char hextab[] = "0123456789ABCDEF"; /*!< Hexadecimal digits table */
+}
+class Mregel {
  public:
   /**
    * @brief Default empty constructor
    */
-  mregel() : mregel_data(std::span<char>()), mregel_data_size(0) {}
-  mregel(std::span<char> data) : mregel_data(data), mregel_data_size(0) {}
+  Mregel() : mregel_data(std::span<char>()), mregel_data_size(0) {}
+  Mregel(std::span<char> data) : mregel_data(data), mregel_data_size(0) {}
   // constructor for initializing the span
   /**
-   * @brief Return size of mregel
+   * @brief Return size of Mregel
    * @return size in characters
    */
   constexpr std::size_t size() const noexcept {
     return mregel_data_size;
   }
   /**
-   * @brief Return underlying size of mregel
+   * @brief Return underlying size of Mregel
    * @return available space
    */
   constexpr std::size_t data_size() const noexcept {
@@ -40,14 +55,21 @@ class mregel {
     return mregel_data.size() - 1;
   }
   /**
-   * @brief Return pointer to mregel data store
-   * @return char pointer to mregel data store
+   * @brief Return pointer to Mregel data store
+   * @return char pointer to Mregel data store
    */
   constexpr char* data() const noexcept {
     return mregel_data.data();
   }
   /**
-   * @brief Return pointer to mregel data store formatted to a c string
+   * @brief Reset Mregel size
+   *
+   */
+  constexpr void reset() noexcept {
+    mregel_data_size = 0;
+  }
+  /**
+   * @brief Return pointer to Mregel data store formatted to a c string
    * @return pointer to c string
    */
   const char* c_str() const noexcept {
@@ -56,25 +78,89 @@ class mregel {
     }
     return mregel_data.data();
   }
-  std::span<char> span() {
+  /**
+   * @brief Return Mregel as std::span
+   * @return a span of characters
+   */
+  std::span<char> span() noexcept {
     if (mregel_data_size == 0)
       return std::span<char>();
     return std::span<char>(mregel_data.data(), mregel_data_size);
   }
   /**
-   * @brief Add C string to mregel
+   * @brief Add C string to Mregel
    * @note will copy up to mregel_data.size()
    * @param c_str C string
    */
-  void add(const char* c_str) {
+  void add(const char* c_str) noexcept {
     while (*c_str != '\0' && mregel_data_size < mregel_data.size()) {
       mregel_data[mregel_data_size] = *c_str;
       mregel_data_size++;
       c_str++;
     }
   }
+  /**
+   * @brief Add character to Mregel
+   * @param c Character to add
+   */
+  void add(char c) noexcept {
+    if (mregel_data_size < mregel_data.size()) {
+      mregel_data[mregel_data_size] = c;
+      mregel_data_size++;
+    }
+  }
+  /**
+   * @brief Add a unsigned 32 bit integer to Mregel
+   * @param n Value to convert and add
+   */
+  void add(std::uint32_t n) {
+    add_num(10, n);
+  }
+  /**
+   * @brief Add a hex value to Mregel
+   * @param n Value to convert and add
+   */
+  void add(Hex n) {
+    add_num(16, n.v);
+  }
+  /**
+   * @brief Add a decimal value to Mregel
+   * @param n Value to convert and add
+   */
+  void add(Dec n) {
+    if (n.v < 0) {
+      add('-');
+      n.v = -n.v;
+    }
+    add_num(10, n.v);
+  }
+  /**
+   * @brief Prints a variable number of arguments
+   * @tparam Ts Types
+   * @param t parameter to print
+   */
+  template <typename... Ts>
+  void add(Ts... t) {
+    (add(t), ...);
+  }
 
  private:
+  /**
+   * @brief Converts an integer to a string and adds it to Mregel
+   * @param base Base of the number
+   * @param n Value to convert a and add
+   */
+  void add_num(const unsigned int base, const uintmax_t n) {
+    // Determine the number of digits we need to print and the maximum divisor
+    uintmax_t divisor = 1;
+    unsigned int p = 0;
+    for (uintmax_t i = n; i >= base; i /= base, p++, divisor *= base)
+      /* Loop to determine size of number */;
+
+    // Print from most-to-least significant digit
+    for (unsigned int i = 0; i <= p; i++, divisor /= base)
+      add(detail::hextab[(n / divisor) % base]);
+  }
   std::span<char> mregel_data;
   std::size_t mregel_data_size;
 };
--- standalone_software/mregel.+23/tests/mregel_tests.cpp
+++ standalone_software/mregel.+23/tests/mregel_tests.cpp
@@ -3,24 +3,36 @@
  *
  * Copyright (c) 2025 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
- * @file main.cpp
- * @brief Main entrypoint for executing minunit tests
+ *
+ * @file mregel_tests.cpp
+ * @brief Tests for the Mregel minimal string class
  */
 #include "minunit.h"
 #include "mregel.hpp"
 #include <cstring>
+#include <array>
 
 std::array<char, 12> hello_world = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
 std::array<char, 15> test_array;
 
+namespace {
+
 MINUNIT_ADD(mregel_test_size, nullptr, nullptr) {
-  mregel mregel_empty;
+  const Mregel mregel_empty;
   MINUNIT_CHECK(mregel_empty.size() == 0);
   MINUNIT_CHECK(mregel_empty.data() == nullptr);
-  mregel mregel_non_empty{test_array};
+  Mregel mregel_non_empty{test_array};
   MINUNIT_CHECK(mregel_non_empty.size() == 0);
   MINUNIT_ASSERT(mregel_non_empty.data() == test_array.data());
   MINUNIT_CHECK(mregel_non_empty.data_size() == 14);
+  mregel_non_empty.add('H');
+  MINUNIT_CHECK(mregel_non_empty.size() == 1);
+  mregel_non_empty.reset();
+  MINUNIT_CHECK(mregel_non_empty.size() == 0);
+}
+
+MINUNIT_ADD(mregel_test_add_cstring, nullptr, nullptr) {
+  Mregel mregel_non_empty{test_array};
   mregel_non_empty.add("");
   MINUNIT_CHECK(mregel_non_empty.size() == 0);
   mregel_non_empty.add("Hello");
@@ -30,7 +42,74 @@ MINUNIT_ADD(mregel_test_size, nullptr, nullptr) {
   mregel_non_empty.add("!");
   MINUNIT_CHECK(mregel_non_empty.size() == 12);
   MINUNIT_CHECK(strcmp(mregel_non_empty.c_str(), "Hello World!") == 0);
-  std::span mregel_span = mregel_non_empty.span();
+  const std::span mregel_span = mregel_non_empty.span();
   MINUNIT_CHECK(mregel_span.size() == 12);
   MINUNIT_CHECK(memcmp(mregel_span.data(), hello_world.data(), 12) == 0);
 }
+
+MINUNIT_ADD(mregel_test_add_char, nullptr, nullptr) {
+  Mregel mregel_non_empty{test_array};
+  mregel_non_empty.add('H');
+  MINUNIT_CHECK(mregel_non_empty.size() == 1);
+  mregel_non_empty.add('e');
+  MINUNIT_CHECK(mregel_non_empty.size() == 2);
+  mregel_non_empty.add('l');
+  MINUNIT_CHECK(mregel_non_empty.size() == 3);
+  mregel_non_empty.add('l');
+  MINUNIT_CHECK(mregel_non_empty.size() == 4);
+  mregel_non_empty.add('o');
+  MINUNIT_CHECK(mregel_non_empty.size() == 5);
+  mregel_non_empty.add(' ');
+  MINUNIT_CHECK(mregel_non_empty.size() == 6);
+  mregel_non_empty.add('W');
+  MINUNIT_CHECK(mregel_non_empty.size() == 7);
+  mregel_non_empty.add('o');
+  MINUNIT_CHECK(mregel_non_empty.size() == 8);
+  mregel_non_empty.add('r');
+  MINUNIT_CHECK(mregel_non_empty.size() == 9);
+  mregel_non_empty.add('l');
+  MINUNIT_CHECK(mregel_non_empty.size() == 10);
+  mregel_non_empty.add('d');
+  MINUNIT_CHECK(mregel_non_empty.size() == 11);
+  mregel_non_empty.add('!');
+  MINUNIT_CHECK(mregel_non_empty.size() == 12);
+  const char *ptr = mregel_non_empty.c_str();
+  MINUNIT_CHECK(strncmp(ptr, "Hello World!", mregel_non_empty.size()) == 0);
+}
+
+MINUNIT_ADD(mregel_test_add_u32, nullptr, nullptr) {
+  Mregel mregel_non_empty{test_array};
+  mregel_non_empty.add(12345U);
+  MINUNIT_CHECK(mregel_non_empty.size() == 5);
+  MINUNIT_CHECK(strncmp(mregel_non_empty.c_str(), "12345", mregel_non_empty.size()) == 0);
+}
+
+MINUNIT_ADD(mregel_test_add_decimal, nullptr, nullptr) {
+  Mregel mregel_non_empty{test_array};
+  mregel_non_empty.add(Dec{-456789});
+  MINUNIT_CHECK(mregel_non_empty.size() == 7);
+  MINUNIT_CHECK(strncmp(mregel_non_empty.c_str(), "-456789", mregel_non_empty.size()) == 0);
+}
+
+MINUNIT_ADD(mregel_test_add_decimal_zero, nullptr, nullptr) {
+  Mregel mregel_non_empty{test_array};
+  mregel_non_empty.add(Dec{0});
+  MINUNIT_CHECK(mregel_non_empty.size() == 1);
+  MINUNIT_CHECK(strncmp(mregel_non_empty.c_str(), "0", mregel_non_empty.size()) == 0);
+}
+
+MINUNIT_ADD(mregel_test_add_hex, nullptr, nullptr) {
+  Mregel mregel_non_empty{test_array};
+  mregel_non_empty.add(Hex{0x1A3B5C7F});
+  MINUNIT_CHECK(mregel_non_empty.size() == 8);
+  MINUNIT_CHECK(strncmp(mregel_non_empty.c_str(), "1A3B5C7F", mregel_non_empty.size()) == 0);
+}
+
+MINUNIT_ADD(mregel_test_variadic_add, nullptr, nullptr) {
+  Mregel mregel_non_empty{test_array};
+  mregel_non_empty.add("Hello", ' ', Dec{-42}, Hex{0x42}, '\n');
+  MINUNIT_CHECK(mregel_non_empty.size() == 12);
+  MINUNIT_CHECK(strncmp(mregel_non_empty.c_str(), "Hello -4242\n", mregel_non_empty.size()) == 0);
+}
+
+}  // namespace
